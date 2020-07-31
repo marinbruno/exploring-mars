15c9af891d334640cc4fa5e972d3f6639acf2f6a
Bruno Marin <bruno_marin@hotmail.com>
2020-07-30 22:52:10 -0300

    Setup the basics of the application

---
+++ src/ExploringMars.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExploringMars.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!🚀🚀🚀🚀");
+        }
+    }
+}
