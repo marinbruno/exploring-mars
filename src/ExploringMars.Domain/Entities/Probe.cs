80f3aaf2fdb6df2a6237724e04cbe9be89aae5b8
Bruno Marin <bruno_marin@hotmail.com>
2020-08-01 16:14:48 -0300

    Add Probe entity

--- src/ExploringMars.Domain/Entities/Probe.cs
+++ src/ExploringMars.Domain/Entities/Probe.cs
@@ -1,7 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ExploringMars.Domain.Entities
 {
     public class Probe
     {
+        public List<Position> PositionHistory { get; }
         
+        public DirectionEnum CurrentDirection { get; set; }
+        
+        public Probe(IReadOnlyCollection<int> coordinates, string startingDirection)
+        {
+            PositionHistory = new List<Position> {new Position(coordinates)};
+            
+            Enum.TryParse(startingDirection, out DirectionEnum direction);
+            CurrentDirection = direction;
+        }
+
+        public void MoveForward(Position plateauLimits)
+        {
+            var currentPosition = PositionHistory.Last();
+            var newPosition = new Position(currentPosition);
+
+            switch (CurrentDirection)
+            {
+                case DirectionEnum.N:
+                    AddNewPosition(plateauLimits, newPosition, currentPosition, true);
+                    break;
+                case DirectionEnum.W:
+                    AddNewPosition(plateauLimits, newPosition, currentPosition, false);
+                    break;
+                case DirectionEnum.S:
+                    AddNewPosition(plateauLimits, newPosition, currentPosition, false);
+                    break;
+                case DirectionEnum.E:
+                    AddNewPosition(plateauLimits, newPosition, currentPosition, true);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void AddNewPosition(Position plateauLimits, Position newPosition, Position currentPosition, bool isMovingTowardsPlateauLimits)
+        {
+            switch (CurrentDirection)
+            {
+                case DirectionEnum.N:
+                case DirectionEnum.S:
+                    newPosition.MoveWidthwise(isMovingTowardsPlateauLimits);
+                    break;
+                case DirectionEnum.E:
+                case DirectionEnum.W:
+                    newPosition.MoveLengthwise(isMovingTowardsPlateauLimits);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            UpdatePositionHistory(newPosition.IsValid(plateauLimits) ? newPosition : currentPosition);
+        }
+
+        public void RotateLeft()
+        {
+            UpdateCurrentDirection(true);
+        }
+
+        public void RotateRight()
+        {
+            UpdateCurrentDirection(false);
+        }
+
+        private void UpdatePositionHistory(Position newPosition)
+        {
+            PositionHistory.Add(newPosition);
+        }
+
+        private void UpdateCurrentDirection(bool isCounterClockwise)
+        {
+            CurrentDirection = CurrentDirection switch
+            {
+                DirectionEnum.N => isCounterClockwise ? DirectionEnum.W : DirectionEnum.E,
+                DirectionEnum.W => isCounterClockwise ? DirectionEnum.S : DirectionEnum.N,
+                DirectionEnum.S => isCounterClockwise ? DirectionEnum.E : DirectionEnum.W,
+                DirectionEnum.E => isCounterClockwise ? DirectionEnum.N : DirectionEnum.S,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
     }
 }
--- tests/ExploringMars.UnitTests/Domain/Entities/ProbeTests.cs
+++ tests/ExploringMars.UnitTests/Domain/Entities/ProbeTests.cs
@@ -1,4 +1,94 @@
-$HEADER$namespace $NAMESPACE$
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExploringMars.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace ExploringMars.UnitTests.Domain.Entities
 {
-  public class $CLASS$ {$END$}
+    public class ProbeTests
+    {
+        private static readonly List<int> ListOfIntegers = new List<int> {1, 3};
+
+        private static readonly Probe ReferenceProbe = new Probe(ListOfIntegers, "N");
+        
+        [Theory]
+        [InlineData("N")]
+        [InlineData("S")]
+        [InlineData("W")]
+        [InlineData("E")]
+        public void Probe_GivenValidCoordinatesAndDirection_ShouldReturnItsPositionHistoryAndDirectionAsExpected(string startingDirection)
+        {
+            var probe = new Probe(ListOfIntegers, startingDirection);
+            Enum.TryParse(startingDirection, out DirectionEnum direction);
+
+            probe.PositionHistory.First().XCoordinate.Should().Be(ListOfIntegers[0]);
+            probe.PositionHistory.First().YCoordinate.Should().Be(ListOfIntegers[1]);
+            probe.CurrentDirection.Should().Be(direction);
+        }
+
+        [Theory]
+        [InlineData(DirectionEnum.N, DirectionEnum.W)]
+        [InlineData(DirectionEnum.W, DirectionEnum.S)]
+        [InlineData(DirectionEnum.S, DirectionEnum.E)]
+        [InlineData(DirectionEnum.E, DirectionEnum.N)]
+        public void
+            RotateLeft_GivenAnyCurrentDirection_ShouldUpdateItsCurrentDirectionAsExpected(DirectionEnum currentDirection, DirectionEnum expectedDirection)
+        {
+            ReferenceProbe.CurrentDirection = currentDirection;
+            
+            ReferenceProbe.RotateLeft();
+
+            ReferenceProbe.CurrentDirection.Should().Be(expectedDirection);
+        }
+        
+        [Theory]
+        [InlineData(DirectionEnum.N, DirectionEnum.E)]
+        [InlineData(DirectionEnum.W, DirectionEnum.N)]
+        [InlineData(DirectionEnum.S, DirectionEnum.W)]
+        [InlineData(DirectionEnum.E, DirectionEnum.S)]
+        public void
+            RotateRight_GivenAnyCurrentDirection_ShouldUpdateItsCurrentDirectionAsExpected(DirectionEnum currentDirection, DirectionEnum expectedDirection)
+        {
+            ReferenceProbe.CurrentDirection = currentDirection;
+            
+            ReferenceProbe.RotateRight();
+
+            ReferenceProbe.CurrentDirection.Should().Be(expectedDirection);
+        }
+
+        [Theory]
+        [InlineData("N")]
+        [InlineData("S")]
+        [InlineData("W")]
+        [InlineData("E")]    
+        public void MoveForward_GivenAnInvalidNewPosition_ShouldAddNewPositionWithTheSameCoordinatesAsTheOriginal(string startingDirection)
+        {
+            var probe = new Probe(new []{0,0}, startingDirection);
+            var plateauLimits = new Position(new []{0,0});
+            
+            probe.MoveForward(plateauLimits);
+
+            probe.PositionHistory.First().XCoordinate.Should().Be(probe.PositionHistory.Last().XCoordinate);
+            probe.PositionHistory.First().YCoordinate.Should().Be(probe.PositionHistory.Last().YCoordinate);
+        }
+
+        [Theory]
+        [InlineData("S", 1, 2)]
+        [InlineData("W", 0, 3)]
+        [InlineData("N", 1, 4)]
+        [InlineData("E", 2, 3)]
+        public void 
+            MoveForward_GivenAValidNewPosition_ShouldAddNewPositionIncrementingItsCoordinateByOne(string startingDirection, int lastXCoordinate, int lastYCoordinate)
+        {
+            var probe = new Probe(ListOfIntegers, startingDirection);
+            var plateauLimits = new Position(new []{5, 5});
+            
+            probe.MoveForward(plateauLimits);
+
+            probe.PositionHistory.Last().XCoordinate.Should().Be(lastXCoordinate);
+            probe.PositionHistory.Last().YCoordinate.Should().Be(lastYCoordinate);
+        }
+    }
 }
